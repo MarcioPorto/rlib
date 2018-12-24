1fe925d4226dee7f4bb2ed5b490de86745a63e42
Marcio Porto <mflporto@gmail.com>
2018-12-23 21:37:29 -0600

    feat: Add DQN test file

---
+++ test/algorithms/dqn_test.py
@@ -0,0 +1,15 @@
+import gym
+
+from rlib.algorithms.dqn.agent import DQNAgent
+from rlib.environments.gym import GymEnvironment
+
+
+def main(seed=0):
+    gym_env = GymEnvironment("Pendulum-v0")
+    dqn_agent = DQNAgent(gym_env.observation_size, gym_env.action_size, seed=seed)
+    gym_env.set_agents([dqn_agent])
+    gym_env.train()
+
+
+if __name__ == "__main__":
+    main()
