ef47708d43fcdf57e42c5c732705525e862f9205
Marcio Porto <mflporto@gmail.com>
2018-12-24 15:56:44 -0600

    fix: Fix runtime errors for Gym environments

--- rlib/environments/gym.py
+++ rlib/environments/gym.py
@@ -26,26 +26,32 @@ class GymEnvironment(BaseEnvironment):
     def action_size(self):
         return self.action_space.shape[0]
 
+    @property
+    def num_env_agents(self):
+        return 1 if len(self.observation_space.shape) == 1 else self.observation_space.shape[0]
+
     def set_agents(self, agents):
         r"""Sets the agents that will be used in this environment."""
         if not isinstance(agents, list):
             agents = [agents]
+        if len(agents) > self.num_env_agents:
+            raise ValueError("Cannot have more agents than the environment can handle.")
         self.agents = agents
 
-    def train(self, num_episodes=100, max_t=None, scores_deque_size=100):
+    def train(self, num_episodes=100, max_t=None, add_noise=True, scores_deque_size=100):
         self.episode_scores = []
 
         for i_episode in range(1, num_episodes+1):
             observation = self.env.reset()
             scores = np.zeros(self.num_agents)
-            self.reset_noise()
+            self.reset_agents()
 
             t = 1
             while True:
                 if max_t and t == max_t + 1:
                     break
 
-                action = self.act(observation, add_noise=True)
+                action = self.act(observation, add_noise=add_noise)
                 next_observation, reward, done, _ = self.env.step(action)
                 self.step(observation, action, reward, next_observation, done)
 
