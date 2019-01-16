7bef3a28c79c86d4a8ce02f479de190b37702977
Marcio Porto <mflporto@gmail.com>
2019-01-15 22:57:09 -0500

    feat: Add logger and gif recorder helper classes

--- rlib/shared/utils.py
+++ rlib/shared/utils.py
@@ -1,10 +1,59 @@
+import os
+
+import imageio
+from tensorboardX import SummaryWriter
+
+
+class Logger:
+    def __init__(self, path, comment=None):
+        """
+        Args:
+            path (str): Location where events will be stored
+            comment (str): Extra description of this experiment
+        """
+        # TODO: Add an option for experiment name?
+        # TODO: Decide whether we want to shadow methods from TX
+
+        self.path = path
+        self.comment = comment
+        self.writer = SummaryWriter(self.path, self.comment)
+
+    def close(self):
+        self.writer.close()
+
+
+class GIFRecorder:
+    """Helper to record GIFs of the environment during training."""
+
+    def __init__(self, path, duration=0.4):
+        """
+        Args:
+            path (str): Location where to store GIFs
+            duration (float): Duration of GIFs
+        """
+        self.path = path
+        self.duration = duration
+        os.makedirs(self.path, exist_ok=True)
+
+    def save_gif(self, filename, frames):
+        """
+        Args:
+            filename (str): Name of file where GIF will be saved
+            frames (list): Environment frames
+        """
+        # TODO: FIX
+        # frames_vector = torch.from_numpy(np.array(frames))
+        # self.logger.add_video("video/episode-{}".format(i_episode), frames_vector)
+
+        imageio.mimsave(os.path.join(self.path, filename), frames, duration=self.duration)
+
+
 def hard_update(local_model, target_model):
     r"""Hard updates model parameters.
 
     θ_target = θ_local
 
-    Params
-    ======
+    Args:
         local_model: PyTorch model (weights will be copied from)
         target_model: PyTorch model (weights will be copied to)
     """
@@ -17,8 +66,7 @@ def soft_update(local_model, target_model, tau):
 
     θ_target = τ * θ_local + (1 - τ) * θ_target
 
-    Params
-    ======
+    Args:
         local_model (PyTorch model): weights will be copied from
         target_model (PyTorch model): weights will be copied to
         tau (float): interpolation parameter
